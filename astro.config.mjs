18a104d3577e48bfc45a29bae761f1e6a31730aa
whereissam <sam135642@gmail.com>
2025-01-12 21:06:05 +0100

    feat: add vercel package

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,28 @@ import netlify from "@astrojs/netlify";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
+import vercel from '@astrojs/vercel';
 
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
 
 import svelte from "@astrojs/svelte";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarcocavallo.com/",
+  output: 'server',
+  adapter: vercel(),
+  site: "https://portfolio-sam-dev.vercel.app/",
   integrations: [
     sitemap(),
-    robotsTxt({
-      sitemap: [
-        "https://gianmarcocavallo.com/sitemap-index.xml",
-        "https://gianmarcocavallo.com/sitemap-0.xml",
-      ],
-    }),
+    // robotsTxt({
+    //   sitemap: [
+    //     "https://gianmarcocavallo.com/sitemap-index.xml",
+    //     "https://gianmarcocavallo.com/sitemap-0.xml",
+    //   ],
+    // }),
     solidJs(),
     UnoCSS({ injectReset: true }),
     icon(),
@@ -29,8 +34,6 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
-  output: "server",
-  adapter: netlify({ edgeMiddleware: true }),
   vite: {
     assetsInclude: "**/*.riv",
   },
